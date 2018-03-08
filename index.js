617369ac460b75b12d3d2110ff82db3e3907e338
Ty Thompson <tylermoko@gmail.com>
2018-03-08 13:01:25 -0500

    adding javascript functionality

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('test');
+var loadPortfolio = document.querySelector('.loadPortfolio');
+var portfolio = document.querySelector('.wrapper__portfolio');
+var about = document.querySelector('.wrapper__about');
+var downButton = document.querySelector(".loadPortfolio");
+var upButton = document.querySelector(".loadWelcome");
+
+loadPortfolio.addEventListener("click",function() {
+    about.style.display = "none";
+    portfolio.style.display = "block";
+    downButton.style.display = "none";
+    upButton.style.display = "block"; 
+});
+
+upButton.addEventListener("click", function() {
+    about.style.display = "block";
+    downButton.style.display = "block";
+    upButton.style.display = "none"; 
+})
